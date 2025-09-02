7085cd4a680477a7080f01a2b9e36b475ccf2572
Teody Seguin <teodyseguin@gmail.com>
2025-09-02 14:27:49 +0800

    Implement the cards component

--- src/js/main.js
+++ src/js/main.js
@@ -12,10 +12,30 @@ function fetchComponentData() {
       ]
     },
     tb: {
-      title: "What does cooking mean",
-      content: "Is it simply applying heat to a food product? A way of making certain food safe to eat? Or a way to create flavour and make food more appealing? This is just part of what Hervé This, the father of molecular gastronomy, has dedicated his life to finding out. We spoke to him to find out what his experiments have told him. And in the process even discovered the secret to cooking the perfect egg...",
-      captionTitle: "The perfect egg",
-      captionContent: "Keep water between 67 and 68°C for a flavourful, tender yolk"
+      field_title: "What does cooking mean",
+      field_content: "Is it simply applying heat to a food product? A way of making certain food safe to eat? Or a way to create flavour and make food more appealing? This is just part of what Hervé This, the father of molecular gastronomy, has dedicated his life to finding out. We spoke to him to find out what his experiments have told him. And in the process even discovered the secret to cooking the perfect egg...",
+      field_caption_title: "The perfect egg",
+      field_caption_content: "Keep water between 67 and 68°C for a flavourful, tender yolk"
+    },
+    cards: {
+      field_title: 'Taste the colours',
+      items: [
+        {
+          field_card_image: 'assets/images/cards/red.webp',
+          field_card_name: 'Red',
+          field_card_content: 'Red foods remind us of berries and soft fruits, so we anticipate a sweet taste.'
+        },
+        {
+          field_card_image: 'assets/images/cards/green.webp',
+          field_card_name: 'Green',
+          field_card_content: 'Fresh, zingy green colours are reminiscent of unripe fruit, promising sour or acid flavours'
+        },
+        {
+          field_card_image: 'assets/images/cards/white.webp',
+          field_card_name: 'White',
+          field_card_content: 'White foods evoke memories of salt and salty flavours, driving the expectation of a savoury treat.'
+        }
+      ]
     }
   };
 }
@@ -36,6 +56,13 @@ function renderTbData() {
   }
 }
 
+function renderCardsData() {
+  const data = fetchComponentData();
+  return {
+    ...data.cards
+  }
+}
+
 // Render the Dynamic Stack.
 const dsSource = document.getElementById("ds--v1").innerHTML;
 const dsTemplate = Handlebars.compile(dsSource);
@@ -49,3 +76,10 @@ const tbTemplate = Handlebars.compile(tbSource);
 const tbContext = renderTbData();
 const tbHtml = tbTemplate(tbContext);
 document.getElementById("tb-container").innerHTML = tbHtml;
+
+// Render Cards.
+const cardsSource = document.getElementById("cards").innerHTML;
+const cardsTemplate = Handlebars.compile(cardsSource);
+const cardsContext = renderCardsData();
+const cardsHtml = cardsTemplate(cardsContext);
+document.getElementById("cards-container").innerHTML = cardsHtml;
